1a2ed8e391aecc09cd0d419897e4baf31cdb496a
Betim Beja <11160171+BetimBeja@users.noreply.github.com>
2021-02-07 14:49:03 +0100

    created InternalStore DTO

---
+++ XrmToolBox.PluginsStore/DTO/InternalStore.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace XrmToolBox.PluginsStore.DTO
+{
+    [DataContract]
+    public class InternalStore
+    {
+        [DataMember(Name = "nuget_feed")]
+        public string NuGetFeed { get; set; }
+
+        [DataMember(Name = "plugins")]
+        public List<XtbPlugin> Plugins { get; set; } = new List<XtbPlugin>();
+    }
+
+    [DataContract]
+    public class InternalStores
+    {
+        [DataMember(Name = "internal_stores")]
+        List<InternalStore> Stores { get; set; } = new List<InternalStore>();
+    }
+}
